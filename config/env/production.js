5e4484905ab4b59f727fbfdeebeafbf88708e434
MattLamont <matthewlamont454@gmail.com>
2017-11-26 16:14:57 -0700

    updated production environment

--- config/env/production.js
+++ config/env/production.js
@@ -17,9 +17,10 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+   models: {
+     connection: 'postgresql',
+     migrate: 'safe'
+   },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
@@ -31,8 +32,8 @@ module.exports = {
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  // log: {
-  //   level: "silent"
-  // }
+  log: {
+     level: "info"
+   }
 
 };
